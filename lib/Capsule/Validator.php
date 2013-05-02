3e68de5be3e51e6d3e1a149a7e713034772fbeec
Keith Pincombe <pincombe@gmail.com>
2013-05-02 17:56:22 +0000

    removed Connection and moved getFactory into Validator class

--- lib/Capsule/Validator.php
+++ lib/Capsule/Validator.php
@@ -8,19 +8,40 @@
 
 namespace Capsule;
 
+use Illuminate\Validation\Factory;
+use Illuminate\Container\Container;
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\Translation\MessageSelector;
+
 /**
  * A simple wrapper class for the Laravel Validation package.
  */
 class Validator {
 
     /**
-     * Passes calls through to the Connection object.
+     * @var Factory  Holds the Validation\Factory object.
+     */
+    private static $_factory = null;
+
+    /**
+     * Passes calls through to the Validation\Factory.
      *
      * @param   string  The method name
      * @param   array   The method parameters sent
      * @return  mixed   The result of the call
      */
-    public static function __callStatic($method, $parameters) {
-        return call_user_func_array(array(Validation\Connection::getFactory(), $method), $parameters);
+    public static function __callStatic($method, $parameters)
+    {
+        return call_user_func_array(array(self::_getFactory(), $method), $parameters);
     }
+
+    private static function _getFactory()
+    {
+        if (is_null(self::$_factory)) {
+            self::$_factory = new Factory(new Translator('en_US', new MessageSelector()), new Container);
+        }
+
+        return static::$_factory;
+    }
+
 }
