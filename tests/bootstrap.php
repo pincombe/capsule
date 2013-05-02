8db93b279e8cd939e9b693cb0fd40cee6067815d
Keith Pincombe <pincombe@gmail.com>
2013-05-02 23:05:05 +0000

    reverted bootstrap back to old method

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,6 @@
 <?php
 
 error_reporting(E_ALL | E_STRICT);
-$loader = require_once __DIR__ . '/../vendor/autoload.php';
-$loader->add('Capsule\\', __DIR__);
-
-
-/*
 if (file_exists($file = __DIR__.'/../../../../autoload.php')) {
     include_once $file;
 }
-*/
